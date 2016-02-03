06143048a7360df642c93e0584c37e71e8091969
[BenjaminSmithOregon] <[benjamin.smith.oregon@gmail.com]>
2016-02-03 10:59:45 -0800

    Adding up to 'finally'

--- Ex37.py
+++ Ex37.py
@@ -43,3 +43,71 @@ while var < 15:
 print "We are all done."
 print "\n"
 
+# definition
+def my_first_definition():
+	print "This is my first definition with zero passed in variables."
+	print "\n"
+
+my_first_definition()
+
+# delete
+list = [1, 2, 3, 4, 5]
+print "List = %s" % list
+
+del list[0]
+
+print "Here is the list with the first number deleted: %s" % list
+print "\n"
+
+# elif
+print "This is a test of the elif statement"
+test = 2
+
+if test == 1:
+	print "Test equals 1."
+elif test == 2:
+	print "Test equals 2."
+else:
+	print "Test equals %s." % test
+print "\n"
+
+# else
+print "This is a test of the else statement."
+test2 = 2
+
+if test2 != 2:
+	print "test2 = %d" % test2
+else:
+	print "test2 = 2."
+print "\n"
+
+# except
+while True:
+	try:
+		x = int(raw_input("Please enter a number: "))
+		break
+	except ValueError:
+		print "That is not a valid number.  Please try again."
+print "\n"
+
+# exec
+print "This is the exec definition."
+exec 'print "Hello all of you who are reading this!" '
+print "\n"
+
+# finally
+def divide_by_zero(x, y):
+	print "This is the finally definition."
+	try:
+		answer = x / y
+	except ZeroDivisionError:
+		print "You have divided by zero."
+	else:
+		print "Your answer is: %d" % answer
+	finally:
+		print "This is the 'finally' statement executing."
+	print "\n"
+
+divide_by_zero(1, 0)
+
+divide_by_zero(2, 1)
